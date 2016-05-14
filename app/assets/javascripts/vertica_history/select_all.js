e83d90e534565dfad99754c9891785f53e34adb3
David Taylor & Josh Dean <instantink+pair+david+josh@hp.com>
2016-05-13 17:26:37 -0700

    made more style changes

--- app/assets/javascripts/vertica_history/select_all.js
+++ app/assets/javascripts/vertica_history/select_all.js
@@ -1,6 +1,6 @@
 window.onload = function(){
   $(".select-all-btn").click(function(){
-    var checkBoxes = $(self).parent().children("input[type=checkbox]");
-    checkBoxes.prop("checked", !checkBoxes.prop("checked"));
+    var checkBoxes = $(this).closest('form').find(".attribute-checkbox input[type=checkbox]");
+    checkBoxes.prop("checked", $(this).prop("checked"));
   });
 };
