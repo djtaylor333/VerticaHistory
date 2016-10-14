3246245d86250914514dc162e9c43cc9bc4e428a
Daniel De La Garza & David Taylor <instantink+pair+daniel+david@hp.com>
2016-10-14 16:37:39 -0700

    Add interface level to make query to vertica

--- lib/vertica_history.rb
+++ lib/vertica_history.rb
@@ -1,7 +1,31 @@
 require "vertica_history/engine"
+require 'rails_admin/config/actions/vertica_history'
+
 module VerticaHistory
   PROJECT_CONFIG = File.join(__dir__, '..', 'config')
+
+  class Interface
+    def self.make_query(query)
+      connection_configurations = {
+          host: Rails.configuration.vertica_host,
+          user: Rails.configuration.vertica_user,
+          password: Rails.configuration.vertica_password,
+          ssl: Rails.configuration.vertica_ssl || true,
+          port: Rails.configuration.vertica_port || 5433,
+          database: Rails.configuration.vertica_database,
+          role: Rails.configuration.vertica_role,
+          search_path: Rails.configuration.vertica_search_path || nil,
+          row_style: Rails.configuration.vertica_row_style || :hash
+      }
+
+      connection = Vertica.connect(connection_configurations)
+      result = connection.query("#{query}")
+      @q = query
+      @columns = result.columns.map { |x| x.name }
+      @rows = result.rows
+      connection.close
+      {columns: @columns, rows: @rows}
+    end
+  end
 end
 
-require 'vertica_history/engine'
-require 'rails_admin/config/actions/vertica_history'
